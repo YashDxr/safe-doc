5d47e83e8505014d4b13a6562fb8947c5db4f645
YashDxr <yashdxr@gmail.com>
2024-02-13 21:33:39 +0530

    upload functionality created firebase storage connected, upload done

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,12 +1,49 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { storage } from "../shared/firebase-config";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
 
 export default function Upload() {
   const [uploadMethod, setUploadMethod] = useState("device");
+  const [document, setDocument] = useState(null);
+  const [name, setName] = useState("");
+  const [docList, setDocList] = useState([]);
+  const nameRef = useRef();
+
+  const handleClick = () => {
+    if (document == null) return;
+    const docRef = ref(storage, `documents/${name + v4()}`);
+    uploadBytes(docRef, document).then(() => {
+      alert("Document uploaded successfully!");
+    });
+  };
+
+  const allDocumentRef = ref(storage, `documents/`);
+  useEffect(() => {
+    listAll(allDocumentRef).then((docs) => {
+      docs.items.forEach((doc) => {
+        getDownloadURL(doc).then((url) => {
+          setDocList(...docList, url);
+        });
+      });
+    });
+  }, []);
 
   return (
     <div className="container mx-auto py-8 flex flex-col items-center">
       <h1 className="text-3xl font-bold m-14">Upload Files</h1>
       <div className="w-1/2 bg-white p-6 rounded-lg shadow-md">
+        <label className="">Name of the document:&nbsp;</label>
+        <input
+          type="text"
+          id="fileName"
+          className="w-2/3 border border-gray-300 p-2 rounded-md mb-4"
+          placeholder="File name..."
+          ref={nameRef}
+          onChange={() => {
+            setName(nameRef.current.value);
+          }}
+        />
         <div className="mb-4">
           <label className="block text-lg font-medium mb-2">
             Select upload method:
@@ -45,8 +82,12 @@ export default function Upload() {
                 Upload from your device:
               </label>
               <input
+                onChange={(e) => {
+                  setDocument(e.target.files[0]);
+                }}
                 type="file"
                 id="fileInput"
+                accept="images/* , .pdf"
                 className="w-full border border-gray-300 p-2 rounded-md mb-4"
                 disabled={uploadMethod !== "device"}
               />
@@ -103,11 +144,17 @@ export default function Upload() {
           </>
         )}
         <div className="flex justify-center">
-          <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
+          <button
+            onClick={handleClick}
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+          >
             Upload
           </button>
         </div>
       </div>
+      <button className="m-4 w-1/3 bg-green-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">
+        Sync
+      </button>
     </div>
   );
 }
--- src/shared/firebase-config.js
+++ src/shared/firebase-config.js
@@ -1,12 +1,9 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getStorage } from "firebase/storage"
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyBnFz5v94AS_4TYFORDL-a4nLbAfDUM_p8",
   authDomain: "oauthsafe.firebaseapp.com",
@@ -17,7 +14,8 @@ const firebaseConfig = {
   measurementId: "G-CSWCWXB66L"
 };
 
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth();
+export const storage = getStorage(app);
